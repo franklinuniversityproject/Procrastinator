f49ff8f060224d171a8abfccade64f44448fb890
NycK7u29KI <sharp29@email.franklin.edu>
2019-06-06 08:16:50 -0400

    another check in

---
+++ Procrastinator/Procrastinator/Foundation/Dal/CourseAdapter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Procrastinator.Foundation.Dal
+{
+    public class CourseAdapter
+    {
+
+    }
+}
---
+++ Procrastinator/Procrastinator/Foundation/Dal/DbProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Procrastinator.Foundation.Dal
+{
+    public class DbProvider
+    {
+        protected DbProvider()
+        {
+            // default Command is a procedure
+        }
+    }
+}
