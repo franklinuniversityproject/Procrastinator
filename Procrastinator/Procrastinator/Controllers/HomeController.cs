6570f81dc3e6482dd118fa9e70be43fa4a026f4c
NycK7u29KI <sharp29@email.franklin.edu>
2019-06-18 19:36:22 -0400

    Fix folder structure

--- Procrastinator/Procrastinator/Controllers/HomeController.cs
+++ Procrastinator/Procrastinator/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Procrastinator.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,18 +9,45 @@ namespace Procrastinator.Controllers
 {
     public class HomeController : Controller
     {
+        private Summer2019 db = new Summer2019();
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Schedule()
+        public ActionResult Courses()
         {
             var model = new Models.CourseSelectionViewModel();
             return View(model);
         }
 
+        [HttpPost]
+        public JsonResult GetCourses(int? subjectTypeId)
+        {
+            var courses = new List<Course>();
+
+            db.Configuration.ProxyCreationEnabled = false;
+            if (subjectTypeId.HasValue)
+            {
+                courses = db.Courses.Where(x => x.SubjectTypeId == subjectTypeId).ToList();
+            }
+            else
+            {
+                courses = db.Courses.ToList();
+            }
+
+            return Json(courses, JsonRequestBehavior.AllowGet);
+        }
+
+
         public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Events()
         {
             return View();
         }
