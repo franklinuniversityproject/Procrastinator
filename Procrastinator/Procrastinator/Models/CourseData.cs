50bd950c22aeebb8e1d923d8d6674323c7640436
Danielle <daniellothecello@gmail.com>
2019-06-21 22:12:00 -0400

    Added Course information classes and Schedule key to home page

---
+++ Procrastinator/Procrastinator/Models/CourseData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Procrastinator.Models
+{
+    public class CourseData
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int CourseCode { get; set; }
+        public int SubjectTypeId { get; set; }
+        public int CreditHours { get; set; }
+        public int DifficultyRating { get; set; }
+
+        public virtual SubjectType SubjectType { get; set; }
+    }
+}
---
+++ Procrastinator/Procrastinator/Shared/CourseBO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using Procrastinator.Models;
+
+namespace Procrastinator.Shared
+{
+    public class CourseBO
+    {
+        private Summer2019 db = new Summer2019();
+
+        public static Func<Course, CourseData> MapCourseToCourseData = m =>
+        {
+            return new CourseData()
+            {
+                Id = m.Id,
+                Description = m.Description,
+                CourseCode = m.CourseCode,
+                SubjectTypeId = m.SubjectTypeId,
+                CreditHours = m.CreditHours,
+                DifficultyRating = m.DifficultyRating
+            };
+        };
+
+        public CourseData GetCourseDataById(int courseID)
+        {
+            return db.Courses
+                .Where(m => m.Id == courseID)
+                .Select(MapCourseToCourseData)
+                .First();
+        }
+
+        public int GetDifficultyByCourseID(int courseID)
+        {
+            return db.Courses
+                .Where(m => m.Id == courseID)
+                .Select(x => x.DifficultyRating)
+                .First();
+        }
+    }
+}
